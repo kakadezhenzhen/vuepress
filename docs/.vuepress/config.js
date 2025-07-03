0739fa67b489c88956c900dd2ecaa59a474d9415
liyuming <1099150760@qq.com>
2025-07-03 19:47:39 +0800

    第一次

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,7 +4,7 @@ import { viteBundler } from '@vuepress/bundler-vite'
 
 export default defineUserConfig({
   lang: 'en-US',
-
+  base: '/vuepress/',
   title: 'VuePress',
   description: 'My first VuePress Site',
 
